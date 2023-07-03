90e6f0eb61fcd42bd5872061589d226deea76c6c
solji <oakikim@ewhain.net>
2023-07-04 00:26:41 +0900

    chore : ecosystem.config.js 파일 수정 완료

--- Backend/ecosystem.config.js
+++ Backend/ecosystem.config.js
@@ -1,22 +1,25 @@
 module.exports = {
-  apps : [{
-    script: 'Backend/server.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
+  apps: [
+    {
+      script: 'Backend/server.js',
+      watch: '.'
+    },
+    {
+      script: 'FrontEnd/build/service-worker.js', // 수정된 부분
+      watch: ['FrontEnd/build/service-worker.js'] // 수정된 부분
+    }
+  ],
+  deploy: {
+    production: {
+      user: 'SSH_USERNAME',
+      host: 'SSH_HOSTMACHINE',
+      ref: 'origin/master',
+      repo: 'GIT_REPOSITORY',
+      path: 'DESTINATION_PATH',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
     }
   }
 };
+

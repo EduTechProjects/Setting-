d1484b4d634982def2ff9535a97fff565e3953d7
solji <oakikim@ewhain.net>
2023-07-04 00:19:21 +0900

    chore : serviceworker 의존성 패키지, 코드작성

---
+++ .history/Backend/FrontEnd/src/service-worker_20230704000259.js
@@ -0,0 +1,64 @@
+// service-worker.js
+import {precacheAndRoute} from 'workbox-preching';
+
+const CACHE_NAME = 'my-cache';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  // 추가 리소스들
+];
+
+// 서비스 워커 설치 이벤트
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 서비스 워커 활성화 이벤트
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => {
+          return cacheName !== CACHE_NAME;
+        }).map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+// 서비스 워커 요청 이벤트
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        // eslint-disable-next-line no-restricted-globals
+        return cache.addAll(self.__WB_MANIFEST);
+      })
+  );
+});
+
+// eslint-disable-next-line no-restricted-globals
+precacheAndRoute(self.__WB_MANIFEST);
---
+++ .history/Backend/FrontEnd/src/service-worker_20230704001634.js
@@ -0,0 +1,47 @@
+import { precacheAndRoute } from 'workbox-precaching';
+
+const CACHE_NAME = 'my-cache';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  // 추가 리소스들
+];
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => {
+          return cacheName !== CACHE_NAME;
+        }).map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+precacheAndRoute(self.__WB_MANIFEST);
+
